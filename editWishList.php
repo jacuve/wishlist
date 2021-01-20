f3ce37b286b1525732ffc73aefe6d70fa211b615
Jose Cuenca <jcuenca@monsol.net>
2021-01-20 23:47:35 +0100

    lesson 6. insert wish adn completed

--- editWishList.php
+++ editWishList.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'includes/WishDB.php';
 session_start();
 if (array_key_exists("user", $_SESSION)) {
     echo "Hello " . $_SESSION['user'];
@@ -9,3 +9,37 @@ else {
    exit;
 }
 ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        
+        <table border="black">
+            <tr>
+                <th>Item</th>
+                <th>Due Date</th>
+            </tr>
+            <?php
+            $wisherID = WishDB::getInstance()->get_wisher_id_by_name($_SESSION['user']);
+            $result = WishDB::getInstance()->get_wishes_by_wisher_id($wisherID);
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr><td>" . htmlentities($row['description']) . "</td>";
+                echo "<td>" . htmlentities($row['due_date']) . "</td></tr>\n";
+            }
+            mysqli_free_result($result);
+            ?>
+        </table>
+        
+        <form name="addNewWish" action="editWish.php">
+            <input type="submit" value="Add Wish">
+        </form>
+        
+        <form name="backToMainPage" action="index.php">
+            <input type="submit" value="Back To Main Page"/>
+        </form>
+
+    </body>
+</html>
